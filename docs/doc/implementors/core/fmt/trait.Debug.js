093790bd3f50c8c84e1a303996da5a27d27522b3
William Watson <williamdwatson@users.noreply.github.com>
2023-09-08 04:11:01 +0000

    Automated documentation update

--- docs/doc/implementors/core/fmt/trait.Debug.js
+++ docs/doc/implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bananagrams_solver":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"bananagrams_solver/enum.LetterUsage.html\" title=\"enum bananagrams_solver::LetterUsage\">LetterUsage</a>"]]
+"bananagrams_solver":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"bananagrams_solver/enum.LetterUsage.html\" title=\"enum bananagrams_solver::LetterUsage\">LetterUsage</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
